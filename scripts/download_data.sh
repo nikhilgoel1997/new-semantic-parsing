afdf7582c0b39ed9306a6039218cc9820e1de86f
Vladislav Lialin <vlialin@google.com>
2020-08-12 16:41:56 +0000

    add Snips download script

--- scripts/download_data.sh
+++ scripts/download_data.sh
@@ -7,3 +7,11 @@ cd data
 curl -JLO http://fb.me/semanticparsingdialog
 unzip semanticparsingdialog  # creates top-dataset-semantic-parsing folder
 cd ..
+
+
+git clone --depth=1 --branch=master https://github.com/snipsco/nlu-benchmark snips
+rm -rf ./snips/.git
+
+cd ../scripts
+
+python process_snips.py --snips-path ../data/snips
